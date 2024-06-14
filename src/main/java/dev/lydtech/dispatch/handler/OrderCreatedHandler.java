c124f4a79206337e6d33cbb7529c6feb0b796915
brandonharshaw <brandonharshaw24@gmail.com>
2024-06-13 21:25:13 -0700

    New service

---
+++ src/main/java/dev/lydtech/dispatch/handler/OrderCreatedHandler.java
@@ -0,0 +1,31 @@
+package dev.lydtech.dispatch.handler;
+
+import dev.lydtech.dispatch.message.OrderCreated;
+import dev.lydtech.dispatch.service.DispatchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OrderCreatedHandler {
+
+    private final DispatchService dispatchService;
+
+    @KafkaListener(
+            id =  "orderConsumerClient",
+            topics = "order.created",
+            groupId = "dispatch.order.created.consumer",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void listen(OrderCreated payload){
+        log.info("received message: payload: "+ payload);
+        try {
+            dispatchService.process(payload);
+        } catch(Exception e){
+            log.error("Processing failure", e);
+        }
+    }
+}
---
+++ src/test/java/dev/lydtech/dispatch/service/DispatchServiceTest.java
@@ -0,0 +1,51 @@
+package dev.lydtech.dispatch.service;
+
+import dev.lydtech.dispatch.message.OrderCreated;
+import dev.lydtech.dispatch.message.OrderDispatched;
+import dev.lydtech.dispatch.util.TestEventData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.springframework.kafka.core.KafkaTemplate;
+import java.util.concurrent.CompletableFuture;
+import static java.util.UUID.randomUUID;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+class DispatchServiceTest {
+
+
+    private DispatchService service;
+    private KafkaTemplate kafkaProducerMock;
+
+    @BeforeEach
+    void setUp() {
+        kafkaProducerMock = mock(KafkaTemplate.class);
+        service = new DispatchService(kafkaProducerMock);
+    }
+
+    @Test
+    void process_success() throws Exception{
+        when(kafkaProducerMock.send(anyString(), ArgumentMatchers.any(OrderDispatched.class))).thenReturn(mock(CompletableFuture.class));
+
+        OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
+        service.process(testEvent);
+        verify(kafkaProducerMock, times(1)).send(eq("order.dispatched"), ArgumentMatchers.any(OrderDispatched.class));
+    }
+
+    @Test
+    void process_ProducerThrowsException(){
+        OrderCreated testEvent = TestEventData.buildOrderCreatedEvent(randomUUID(), randomUUID().toString());
+
+        doThrow(new RuntimeException("Producer failure")).when(kafkaProducerMock).send(eq("order.dispatched"), ArgumentMatchers.any(OrderDispatched.class));
+
+        Exception exception = assertThrows(RuntimeException.class, () -> service.process(testEvent));
+
+        verify(kafkaProducerMock, times(1)).send(eq("order.dispatched"), ArgumentMatchers.any(OrderDispatched.class));
+        assertThat(exception.getMessage(), equalTo("Producer failure"));
+    }
+}
