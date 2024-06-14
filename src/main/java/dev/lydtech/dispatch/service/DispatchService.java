b0c6b4c8943d9658949d7780897ff8d380e1d3fc
brandonharshaw <brandonharshaw24@gmail.com>
2024-06-13 23:32:57 -0700

    producer for sending order tracking status request to dispatch.tracking topic

--- src/main/java/dev/lydtech/dispatch/service/DispatchService.java
+++ src/main/java/dev/lydtech/dispatch/service/DispatchService.java
@@ -1,5 +1,6 @@
 package dev.lydtech.dispatch.service;
 
+import dev.lydtech.dispatch.message.DispatchPrepared;
 import dev.lydtech.dispatch.message.OrderCreated;
 import dev.lydtech.dispatch.message.OrderDispatched;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class DispatchService {
 
     private static final String ORDER_DISPATCHED_TOPIC = "order.dispatched";
+    private static final String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
     private final KafkaTemplate<String,Object> kafkaProducer;
 
     public void process(OrderCreated orderCreated) throws Exception{
@@ -18,5 +20,10 @@ public class DispatchService {
                 .orderId(orderCreated.getOrderId())
                 .build();
         kafkaProducer.send(ORDER_DISPATCHED_TOPIC, orderDispatched).get();
+    }
+
+    public void processTrackingStatus(DispatchPrepared dispatchPrepared) throws Exception{
+
+        kafkaProducer.send(DISPATCH_TRACKING_TOPIC, dispatchPrepared).get();
     }
 }
