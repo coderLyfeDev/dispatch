908cec933545224cdfaa70ecbbad7b92e17ac12f
brandonharshaw <brandonharshaw24@gmail.com>
2024-06-19 21:20:37 -0700

    added code for multiple instances and consumer groups.

--- src/main/java/dev/kafka/dispatch/service/DispatchService.java
+++ src/main/java/dev/kafka/dispatch/service/DispatchService.java
@@ -4,17 +4,23 @@ import dev.kafka.dispatch.message.DispatchPrepared;
 import dev.kafka.dispatch.message.OrderCreated;
 import dev.kafka.dispatch.message.OrderDispatched;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class DispatchService {
 
     private static final String ORDER_DISPATCHED_TOPIC = "order.dispatched";
     private static final String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
     private final KafkaTemplate<String,Object> kafkaProducer;
-
+    private static final UUID APPLICATION_ID = randomUUID();
     public void process(OrderCreated orderCreated) throws Exception{
 
         DispatchPrepared dispatchPrepared = DispatchPrepared.builder()
@@ -24,7 +30,11 @@ public class DispatchService {
 
         OrderDispatched orderDispatched = OrderDispatched.builder()
                 .orderId(orderCreated.getOrderId())
+                .processedBy(APPLICATION_ID)
+                .notes("Dispatched: "+ orderCreated.getItem())
                 .build();
         kafkaProducer.send(ORDER_DISPATCHED_TOPIC, orderDispatched).get();
+
+        log.info("Sent Message: orderID: " + orderCreated.getOrderId() + " - processedByID: "+APPLICATION_ID);
     }
 }
